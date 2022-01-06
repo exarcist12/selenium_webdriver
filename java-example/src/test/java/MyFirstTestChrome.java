c77b8828b7e57554ef93ff79a459bb030211f682
exarcist12 <exarcist12@gmail.com>
2022-01-06 16:28:21 +0400

    task15

--- java-example/src/test/java/MyFirstTestChrome.java
+++ java-example/src/test/java/MyFirstTestChrome.java
@@ -5,8 +5,14 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -17,8 +23,9 @@ public class MyFirstTestChrome {
     private WebDriverWait wait;
 
     @Before
-    public void start(){
-        chromeDriver = new ChromeDriver();
+    public void start() throws MalformedURLException {
+        WebDriver chromeDriver = new RemoteWebDriver(new URL("http://192.168.56.1:4443/wd/hub"), new ChromeOptions());
+ //       chromeDriver = new ChromeDriver();
         wait = new WebDriverWait(chromeDriver, 10);
     }
 
